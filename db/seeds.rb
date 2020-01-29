d064b108fbe2ac3aaf2f30a626102471538b228a
Yenisbel Valle <yenisbelv@gmail.com>
2020-01-28 21:32:33 -0800

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 User.destroy_all
-Bench.destroy_all
+# Bench.destroy_all
 
 user1 = User.create(email: 'test1', password: 'password1', full_name: 'Cindy Loop')
 user2 = User.create(email: 'user@abc.com', password: 'password1', full_name: 'Louise Loop')
@@ -22,6 +22,6 @@ user3 = User.create(email: 'test3', password: 'password1', full_name: 'Steve Loo
 
 
 
-bench1 = Bench.create(description: 'bench1', lat: 1.1, lng: 1.1)
-bench2 = Bench.create(description: 'bench2', lat: 2.2, lng: 2.2)
-bench3 = Bench.create(description: 'bench3', lat: 3.3, lng: 3.3)
+# bench1 = Bench.create(description: 'bench1', lat: 1.1, lng: 1.1)
+# bench2 = Bench.create(description: 'bench2', lat: 2.2, lng: 2.2)
+# bench3 = Bench.create(description: 'bench3', lat: 3.3, lng: 3.3)
